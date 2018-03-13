7689ebf14053032d7c4f84ae3a809657728730c0
John Vandenberg <jayvdb@gmail.com>
2018-03-14 03:15:20 +0700

    icons.json: Add more social sites
    Upgrade to font-awesome 5.0.0 and add more icons that
    can be used.

--- lib/jekyll_social_icons/social_icons_styles.rb
+++ lib/jekyll_social_icons/social_icons_styles.rb
@@ -15,7 +15,7 @@ module Jekyll
       private
 
       def header_styles
-"""<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+"""<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/font-awesome/5.0.0/css/font-awesome.min.css'>
 <style>
 #{style_document}
 </style>"""
